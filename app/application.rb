ff0e90b631f45a6e2e1ebde1e0b745e2e8b34200
github username <github email address>
2020-01-05 14:50:41 -0500

    pause1

--- app/application.rb
+++ app/application.rb
@@ -15,8 +15,9 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      @@cart.empty?
-        puts "Your cart is empty"
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      end
     else
       resp.write "Path Not Found"
     end
